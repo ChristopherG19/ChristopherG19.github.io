d54848a28d00d7f7bf4f1dfd3d0d4ca38bda7019
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-05-28 17:49:06 -0600

    Actualización V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import NavBar from './NavBar/Navbar'
+import AboutSection from './Sections/About'
+import PortfolioSection from './Sections/Portfolio'
+import ContactSection from './Sections/Contact'
 
 const App = () => {
   
   return (
     <div className="App">
+      <a className="btnHome" type="a" href='#top'><i class="fa fa-home"> </i>Home</a>
       <NavBar />
-      <div id='first-class' className='principal-content'>a</div>
-      <div id='second-class' className='secundary-content'>b</div> 
-      <div id='third-class' className='third-content'>c</div> 
+      <div className='content'>
+        <AboutSection />
+        <PortfolioSection />
+        <ContactSection />
+      </div>
     </div>
   )
 }
